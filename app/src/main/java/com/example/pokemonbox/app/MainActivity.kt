4fd0bd1ca93017a7b6bafdbdfe5dd04827a217eb
mario-roberto-fortunato <mariorobertofortunato@gmail.com>
2026-02-14 13:48:17 +0100

    feat: Add Material Icons Extended dependency
    Adds the `androidx.compose.material.icons.extended` library to the project for use in Jetpack Compose.
    
    - Includes the dependency in `build.gradle.kts`.
    - Defines the dependency in `libs.versions.toml`.

--- app/src/main/java/com/example/pokemonbox/app/MainActivity.kt
+++ app/src/main/java/com/example/pokemonbox/app/MainActivity.kt
@@ -4,11 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.pokemonbox.app.ui.screens.MainScreen
 import com.example.pokemonbox.app.ui.theme.PokemonBoxTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
